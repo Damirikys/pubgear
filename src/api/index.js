6b8151f669018b001172a366c9675cd9147de1d8
Damir Shaikhislamov <jeminay.dev@gmail.com>
2018-01-25 00:26:34 +0500

    Пример работы с api, получение user_id и ranked_stats

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,16 @@
-export default {
+const BASE_URL = 'https://pubg.op.gg'
 
+class APIService {
+  playerIdByName = async(name) => {
+    const response = await fetch(`${BASE_URL}/search/?playerName=${name}`)
+    const responseText = await response.text()
+    return responseText.match(/data-user_id="(\w+)"/)[1]
+  }
+
+  rankedStatsById = async(id) => {
+    const response = await fetch(`${BASE_URL}/api/users/${id}/ranked-stats?season=2018-01&server=eu&queue_size=1&mode=tpp`)
+    return await response.json()
+  }
 }
+
+export default new APIService()
